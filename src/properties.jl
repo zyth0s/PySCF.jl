70fab248f36cc3a5acc54ba27b41e57a5805e10d
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-08-02 12:26:52 +0200

    Tested number of electron pairs.

--- src/properties.jl
+++ src/properties.jl
@@ -98,6 +98,36 @@ function fock_dirac_denmat(C,S,D)
 end
 
 """
+Nj^2. McWeeny convention (wo. 1/2)
+# Arguments
+- D in AO basis
+- S is ovlp in AO basis
+"""
+function number_pairs_j(D,S)
+   #@tensor scalar(D[i,j] * D[k,l] * S[i,j] * S[k,l])
+   #@tensor scalar( (D[i,j] * S[i,j]) * (D[k,l] * S[k,l]))
+   @tensor scalar(D[i,j] * S[i,j] * D[k,l] * S[k,l])
+end
+
+"""
+Nxc. McWeeny convention (wo. 1/2)
+
+# Arguments
+- D in AO basis
+- S is ovlp in AO basis
+"""
+function number_pairs_xc(D,S)
+   #@tensor 0.5scalar(D[i,j] * D[k,l] * S[i,l] * S[k,j])
+   #@tensor 0.5scalar(D[i,j] * D[k,l] * S[l,i] * S[k,j]) # D is symmetric
+   #@tensor 0.5scalar(D[i,j] * S[l,i] * D[k,l] * S[k,j]) # D is symmetric
+   @tensor 0.5scalar(D[i,j] * S[i,l] * D[k,l] * S[k,j]) # j ↔ l
+end
+
+function number_pairs(D,S)
+   number_pairs_j(D,S) - number_pairs_xc(D,S)
+end
+
+"""
 2RD
 """
 function second_reduced_density(rdm1)
@@ -114,21 +144,30 @@ end
 2RDM
 """
 function second_denmat(D)
+   # Choose convention: McWeeny or Löwdin (0.5 factor)
    # Clearly explained in sections 11.15, 13.3 of Janos Angyan notes
    #rdm2 = zeros(nsmo,nsmo,nsmo,nsmo)
    #for i in 1:nao, j in 1:nao, k in 1:nao, l in 1:nao
-   rdm2 = zeros(size(D)...,size(D)...)
-   for (i,j,k,l) in Tuple.(CartesianIndices(rdm2))
+   #for i in 1:nao,j in 1:nao,k in 1:nao,l in 1:nao
       # 1st RHS is the Coulomb component ∫ = N^2
       # 2nd RHS is the exchange component ∫ = N
       #rdm2[l,k,i,j] =     D[l,i] * D[k,j] - D[l,j] * D[k,i] # eq. (338) Janos
-      rdm2[i,j,k,l] = 0.5(D[i,j] * D[k,l] - D[i,l] * D[k,j]) # eq. (6) J. of Math. Chem. 28, Nos. 1–3, 2000
-   end
-   #for i in 1:nao,j in 1:nao,k in 1:nao,l in 1:nao
       #lk, ij = index(l,k), index(i,j)
       #rdm2[l,k,i,j] = D[l,i]*D[j,k] - D[l,j]*D[k,i]
       #rdm2[l,j,i,k] = D[l,i]*D[k,j] - D[l,k]*D[j,i]
+      #rdm2[i,j,k,l] = 0.5(D[i,j] * D[k,l] - D[i,l] * D[k,j]) # eq. (6) J. of Math. Chem. 28, Nos. 1–3, 2000
    #end
-   rdm2
+   #rdm2 = zeros(size(D)...,size(D)...)
+   #for (i,j,k,l) in Tuple.(CartesianIndices(rdm2))
+   #   rdm2[i,j,k,l] = D[i,j] * D[k,l] - D[i,l] * D[k,j]
+   #end
+   #rdm2
+   @tensor rdm2[i,j,k,l] := D[i,j] * D[k,l] - D[i,l] * D[k,j]
 end
 
+"""
+2RDM Coulomb part (J to distinguish from c for correlation)
+"""
+function second_denmat_j(D)
+   @tensor rdm2_j[i,j,k,l] := D[i,j] * D[k,l]
+end
--- test/test_properties.jl
+++ test/test_properties.jl
@@ -1,7 +1,10 @@
 
 
 #FIXME: PyObject not found when pyscf only
-import PySCF: scf_rhf, dipole_moment, population_analysis #, pyscf
+import PySCF: scf_rhf #, pyscf
+import PySCF: dipole_moment
+import PySCF: population_analysis
+import PySCF: number_pairs_j, number_pairs_xc, number_pairs
 
 # Workaround: repeat the imports.
 using PyCall: pyimport
@@ -57,4 +60,37 @@ end
 
    QMulliken_pyscf = mf.analyze(verbose=0,ncol=10, with_meta_lowdin=false, digits=9)[1][2]
    @test isapprox(QMulliken, QMulliken_pyscf, atol=1e-7) # a.u.
+end
+
+@testset "SCF RHF Number of electron pairs: H₂O" begin
+   mol = pyscf.gto.Mole()
+   mol.build(verbose = 0,
+             basis = "sto-3g",
+             atom = """8 0  0.    0
+                       1 0 -0.757 0.587
+                       1 0  0.757 0.587""",
+            )
+
+   mf = pyscf.scf.RHF(mol)
+
+   rhf         = scf_rhf(mol,verbose=false)
+   e_rhf_pyscf = mf.kernel()
+
+   s     = mol.intor("cint1e_ovlp_sph")
+
+   C = rhf.C
+   nocc = mol.nelectron ÷ 2
+   mocc = C[:,1:nocc]
+   P  = mocc * mocc'
+   # In RHF, D = 2P, an occupied spatial orbital means 2 elec, α and β
+   # PS is a "correct" representation of the 1RDM in AO basis
+
+   s = mol.intor("int1e_ovlp")
+   pairs_j  = number_pairs_j(2P,s)
+   pairs_xc = number_pairs_xc(2P,s)
+   pairs    = number_pairs(2P,s)
+   nelec = mol.nelectron
+   @test pairs_j ≈ nelec^2
+   @test pairs_xc ≈ nelec
+   @test pairs ≈ nelec*(nelec-1)
 end
